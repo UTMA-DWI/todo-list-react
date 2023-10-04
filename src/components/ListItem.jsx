b216568d79deeac7637766c260ded697c836c764
Alex HM <alex13pks@gmail.com>
2023-10-03 20:04:18 -0600

    Added todo components

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function ListItem({ text, onDelete }) {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = () => {
+    // if (checked == true) {
+    //     setChecked(false);
+    // } else {
+    //     setChecked(true);
+    // }
+    setChecked(!checked);
+  };
+  return (
+    <li
+      className={`bg-indigo-800 p-2 ${
+        checked ? "text-green-400 line-through" : null
+      }`}
+    >
+      {text} <input type="checkbox" checked={checked} onChange={handleCheck} />
+    </li>
+  );
+}
+
+export default ListItem;
---
+++ src/pages/Todo.jsx
@@ -0,0 +1,15 @@
+import ListItem from "../components/ListItem";
+
+function Todo() {
+  return (
+    <div>
+      <ul>
+        <ListItem text="Item 1" />
+        <ListItem text="Item 2" />
+        <ListItem text="Item 3" />
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
